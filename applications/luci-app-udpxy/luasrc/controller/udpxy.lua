f05a30cf86cf77c82964a004a6285d25367d3369
Yonsm <Yonsm@qq.com>
2019-09-22 01:15:10 +0800

    Revert udpxy

--- applications/luci-app-udpxy/luasrc/controller/udpxy.lua
+++ applications/luci-app-udpxy/luasrc/controller/udpxy.lua
@@ -8,7 +8,7 @@ function index()
 		return
 	end
 
-	local page = entry({"admin", "services", "udpxy"}, cbi("udpxy"), _("UdPxy"), 100)
+	local page = entry({"admin", "services", "udpxy"}, cbi("udpxy"), _("udpxy"), 100)
 	page.dependent = true
 
 end
